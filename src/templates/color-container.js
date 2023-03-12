724a3d1cc5366721f0df1138c626a63d9de93f09
Gaetan <72258504+gaetan-hexadog@users.noreply.github.com>
2023-03-12 14:40:08 +0100

    fix(template): update export template

--- src/templates/color-container.js
+++ src/templates/color-container.js
@@ -1,12 +1,12 @@
 module.exports = (data) => {
     return `<div>
         <div class="flex flex-col space-y-3 sm:flex-row text-xs sm:space-y-0 sm:space-x-4">
-            <div class="w-16 shrink-0">
-                <div class="h-10 flex flex-col justify-center">
+            <div class="w-24 shrink-0">
+                <div class="h-12 flex flex-col justify-center">
                     <div class="text-sm font-semibold text-slate-900 dark:text-slate-200">${data.name}</div>
                 </div>
             </div>
-            <div class="min-w-0 flex-1 grid grid-cols-5 2xl:grid-cols-10 gap-x-4 gap-y-3 2xl:gap-x-2">
+            <div class="min-w-0 grid grid-cols-10">
                 ${data.shades}
             </div>
         </div>
--- src/templates/color-palette.js
+++ src/templates/color-palette.js
@@ -14,7 +14,9 @@ module.exports = (data) => {
 
     <body>
         <main class="relative mt-52 md:mt-32">
-            <div class="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">${data.palette}</div>
+            <div class="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8 space-y-6">
+                ${data.palette}
+            </div>
         </main>
     </body>
 </html>`;
